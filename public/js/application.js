3cd5ac64804e02855fbbc4e84021962db3edd379
Kirstin "Kir" Jarchow <kirstinjarchow@gmail.com>
2014-07-24 14:29:39 -0700

    initial widget tinkering. going to have to give a good pause from the seeking to initialize the song

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
+  var widgetIframe = document.getElementById('sc-widget')
+  var widget = SC.Widget(widgetIframe);
+  widget.bind(SC.Widget.Events.READY, function() {
+    widget.bind(SC.Widget.Events.PLAY, function() {
+      widget.seek(10000)
+      // get information about currently playing sound
+      widget.getCurrentSound(function(currentSound) {
+        console.log('sound ' + currentSound.get('') + 'began to play');
+      });
+    });
+    // get current level of volume
+    widget.getVolume(function(volume) {
+      console.log('current volume value is ' + volume);
+    });
+    // set new volume level
+    widget.setVolume(50);
+    // get the value of the current position
+  });
+}());
