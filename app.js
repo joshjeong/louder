2a4187613ae27e8da615ef2b90450b5eb860227d
& <pair+ronishak.vmitchell@devbootcamp.com>
2014-07-25 11:47:58 -0700

    Created the express skeleton for the louder app
    Used yeoman to generate the skeleton and manage our dependencies

--- app.js
+++ app.js
@@ -1,25 +1,69 @@
-// This is the main file of our chat app. It initializes a new 
-// express.js instance, requires the config and routes files
-// and listens on a port. Start the application by running
-// 'node app.js' in your terminal
+'use strict';
+
+/*
+ * Express Dependencies
+ */
+var express = require('express');
+var app = express();
+var port = 3000;
+
+/*
+ * Use Handlebars for templating
+ */
+var exphbs = require('express3-handlebars');
+var hbs;
+
+// For gzip compression
+app.use(express.compress());
+
+/*
+ * Config for Production and Development
+ */
+if (process.env.NODE_ENV === 'production') {
+    // Set the default layout and locate layouts and partials
+    app.engine('handlebars', exphbs({
+        defaultLayout: 'main',
+        layoutsDir: 'dist/views/layouts/',
+        partialsDir: 'dist/views/partials/'
+    }));
+
+    // Locate the views
+    app.set('views', __dirname + '/dist/views');
+
+    // Locate the assets
+    app.use(express.static(__dirname + '/dist/assets'));
+
+} else {
+    app.engine('handlebars', exphbs({
+        // Default Layout and locate layouts and partials
+        defaultLayout: 'main',
+        layoutsDir: 'views/layouts/',
+        partialsDir: 'views/partials/'
+    }));
+
+    // Locate the views
+    app.set('views', __dirname + '/views');
+
+    // Locate the assets
+    app.use(express.static(__dirname + '/assets'));
+}
+
+// Set Handlebars
+app.set('view engine', 'handlebars');
 
 
-var express = require('express'),
-	app = express();
 
-// This is needed if the app is run on heroku:
+/*
+ * Routes
+ */
+// Index Page
+app.get('/', function(request, response, next) {
+    response.render('index');
+});
 
-var port = process.env.PORT || 8080;
 
-// Initialize a new socket.io object. It is bound to 
-// the express app, which allows them to coexist.
-
-var io = require('socket.io').listen(app.listen(port));
-
-// Require the configuration and the routes files, and pass
-// the app and io as arguments to the returned functions.
-
-require('./config')(app, io);
-require('./routes')(app, io);
-
-console.log('Your application is running on http://localhost:' + port);
+/*
+ * Start it up
+ */
+app.listen(process.env.PORT || port);
+console.log('Express started on port ' + port);
