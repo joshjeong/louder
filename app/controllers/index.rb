31109a1da9776840f8182d6e0e2723be19f53a5b
Kirstin "Kir" Jarchow <kirstinjarchow@gmail.com>
2014-07-25 12:22:33 -0700

    Seems that my brain has a logic error. Now there is only a one second delay to playback. Go team! Have to wait until we have everything implemented together to test more, however

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,5 @@
 get '/' do
-  @fakeHostTime = Time.now.to_f * 1000 - 15000
-  @fakeHostProgress = 15000
+  @fakeHostTime = Time.now.to_f * 1000 - 20000
+  @fakeHostProgress = 5000
   erb :index
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,11 +1,12 @@
-function eventAlarm(minutes, seconds, milliseconds, cb) {
+// Useless function
+/* function eventAlarm(minutes, seconds, milliseconds, cb) {
   (function loop() {
       var now = new Date();
       if (now.getMinutes() === minutes && now.getSeconds() === seconds && now.getMilliseconds() >= milliseconds) {
         cb();
       } else {
-        console.log(new Date().getMilliseconds())
         //recursively calls self to the delay
         setTimeout(loop, 1); }
   })();
 }
+*/
