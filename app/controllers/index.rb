b99c2bdb0fc8ce0eb07542c2c9be261eae1534ec
Kirstin "Kir" Jarchow <kirstinjarchow@gmail.com>
2014-07-25 15:00:13 -0700

    updating time again

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,7 +2,7 @@ require 'time'
 
 get '/' do
   # to float and times 1000 to turn it into Javascript's time format
-  @fakeHostTime = 1406324808524.9338 #Time.now.to_f * 1000 - 20000
+  @fakeHostTime = 1406325594471.7861 #Time.now.to_f * 1000 - 20000
   @fakeHostProgress = 5000
   erb :index
 end
