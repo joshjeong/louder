3238b917d15298fe5ab545a5ede3ad08ea00de66
Kirstin "Kir" Jarchow <kirstinjarchow@gmail.com>
2014-07-25 12:05:10 -0700

    added eventAlarm function, which will trigger an event passed into it at a specific minute, second, and millisecond. It's recursive, so be careful. Typically the delay between iterations is 5 milliseconds, but it varies from 4-8. Hopefully this isn't too problematic.

---
+++ public/js/application.js
@@ -0,0 +1,11 @@
+function eventAlarm(minutes, seconds, milliseconds, cb) {
+  (function loop() {
+      var now = new Date();
+      if (now.getMinutes() === minutes && now.getSeconds() === seconds && now.getMilliseconds() >= milliseconds) {
+        cb();
+      } else {
+        console.log(new Date().getMilliseconds())
+        //recursively calls self to the delay
+        setTimeout(loop, 1); }
+  })();
+}
