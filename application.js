9ffd16e90e40c4a9d87f4cd3c6ab04bedc87764e
Jeff Kynaston & josh jeong <pair+jeffkynaston.joshjeong@devbootcamp.com>
2014-07-24 22:19:39 -0700

    middle of creating search functionality

--- application.js
+++ application.js
@@ -5,9 +5,11 @@ pController.bindListeners();
 })
 
 Player = {}
+Search = {}
 
 Player.Controller = function(view) {
   this.view = view
+  this.searchQuery = new Player.SearchQuery(this)
 
   var widgetIframe = this.view.getWidget()
   widget = SC.Widget(widgetIframe);
@@ -37,17 +39,24 @@ Player.Controller = function(view) {
   Player.Controller.prototype = {
     bindListeners: function() {
       var form = this.view.getForm()
-      form.on('submit', this.queueSong.bind(this))
+      form.on('submit', this.displaySearchQuery.bind(this))
       // debugger
       widget.bind(SC.Widget.Events.FINISH, this.view.renderNextSong.bind(this.view))
       },
       // widget.bind(SC.Widget.Events.PLAY_PROGRESS, this.view.renderNextSong.bind(this.view))
       // },
-    queueSong: function(event) {
+    displaySearchQuery: function(event) {
       event.preventDefault()
-      url = this.view.getInput()
-      // use serialize toget form info
-      // send to player.view to store in queue
+      searchParameter = this.view.getInput()
+      this.searchQuery.getTracks(searchParameter)
+      // debugger
+      // send searchParameter to Player.SearchQuery object to hit soundcloud api
+      // returns arry of song objects with artwork urls and song urls
+      // create a gallery of thumnails with song titles and artists
+
+    },
+
+    queueSong: function() {
       this.view.queue.push(url)
       this.view.clearInput(url)
     }
@@ -92,3 +101,74 @@ Player.View.prototype = {
   }
 
 }
+
+
+
+Player.SearchQuery = function(controller){
+  this.controller = controller
+}
+
+Player.SearchQuery.prototype = {
+  getTracks: function(searchParameter) {
+    var allTracks = []
+    SC.initialize({
+      client_id: 'd8eb7a8be0cc38d451a51d4d223ee84b'
+    });
+    SC.get('http://api.soundcloud.com/tracks', { q: 'zedd' }, function(tracks) {
+      debugger
+    });
+  }
+}
+
+
+//////////////////////======================================
+
+// Search.Controller = function(view) {
+//   this.view = view
+// }
+
+// Search.Controller.prototype = {
+//   bindListeners: function() {
+//       var form = this.view.getForm()
+//       form.on('submit', this.queueSong.bind(this))
+//       // debugger
+//       widget.bind(SC.Widget.Events.FINISH, this.view.renderNextSong.bind(this.view))
+//       },
+//       // widget.bind(SC.Widget.Events.PLAY_PROGRESS, this.view.renderNextSong.bind(this.view))
+//       // },
+//     queueSong: function(event) {
+//       event.preventDefault()
+//       url = this.view.getInput()
+//       // use serialize toget form info
+//       // send to player.view to store in queue
+//       this.view.queue.push(url)
+//       this.view.clearInput(url)
+//     }
+// }
+
+// Search.View = function() {
+//   this.queue = []
+// }
+
+// Search.View.prototype = {
+
+//   getForm: function() {
+//     return $('#search')
+//   },
+//   getInput: function() {
+//     return $('#search').find('input').val()
+//   },
+//   clearInput: function(url) {
+//     $('#search').find('input').first().val('')
+//     $('#flash-message').text("" + url + " Was Just Added")
+//     $('#flash-message').fadeIn(1000)
+//     $('#flash-message').fadeOut(3000)
+//   },
+
+//   renderNextSong: function() {
+//     // debugger
+//     var url = this.queue.splice(0,1)[0]
+//     var html = '<iframe id="sc-widget" src="https://w.soundcloud.com/player/?url=' + url + '&sharing=false&download=false&buying=false&liking=false&show_comments=false&show_playcount=true&show_user=false&show_artwork=true&visual=true" width="100%" height="465" scrolling="no" frameborder="no"></iframe>'
+//     $('#frame-holder').html(html)
+//   }
+// }
