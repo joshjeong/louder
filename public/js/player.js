9bedcd88fe5425cf330a9c18f18fff72d1efbe7e
Val Mitchell <ibeatnorris@gmail.com>
2014-07-28 16:09:34 -0700

    cleaned some code

--- public/js/player.js
+++ public/js/player.js
@@ -9,7 +9,6 @@ $( document ).ready(function(){
   globalCurrentSongUrl = ""
 });
   timestampData = {}
-  // setTimeout(function(){pController.playTrack()}, 3000);
 
   // D3 OBJECT --------------------------------------
 
@@ -45,7 +44,6 @@ $( document ).ready(function(){
 
     d3.event.preventDefault();
   }
-
 
 Player = {}
 Player.Controller = function() {
@@ -96,33 +94,10 @@ Player.Controller = function() {
     $('#pause-button').on('click', this.pauseTrack)
   }
 
-  // this.loadSongFromURL = function(event) {
-  //   event.preventDefault()
-  //   var trackUrl = $(event.target).find('input').eq(0).val()
-  //   console.log('this is the track url')
-  //   console.log(trackUrl)
-  //   $.get('http://api.soundcloud.com/resolve.json?url=' + trackUrl + '&client_id=d8eb7a8be0cc38d451a51d4d223ee84b',
-  //     function (song) {
-  //       _this.currentSongUri = song.uri;
-  //       SC.stream(_this.currentSongUri, {
-  //         id: "louderPlayer",
-  //         onplay: function(){
-  //           this.onPosition(1, _this.sendHostTimestamps)
-  //         }},
-  //         function(sound){
-  //           _this.currentSong = sound
-  //         })
-  //       socket.emit('hostPickedSong', {song: _this.currentSongUri})
-  //     });
-  // }
-
   this.bufferGuestTrack = function () {
     socket.emit('userClickedConnect', {id: socket.io.engine.id, playing: true})
       $('#guest-playing').show()
       $('#connect-button').hide()
-      // send message to server to grab song time from host
-      // new function, server sends time back to guest to play
-      console.log('this is the stream url! is it getting through?')
       console.log(globalCurrentSongUrl)
       SC.stream(globalCurrentSongUrl, function(sound){
         debugger
@@ -135,46 +110,31 @@ Player.Controller = function() {
         _this.currentSong.toggleMute()
       })
     }
-
-    this.sendHostTimestamps = function(){
-      socket.emit('hostClickedPlay',
-        {timestamp: Date.now(), songProgress: _this.currentSong.position}
-        )
-        var timeToPlay = (guestTimeStamp - hostTimeStamp + hostProgress)
-        _this.currentSong.setPosition((timeToPlay+10000)).play()
-        _this.currentSong.toggleMute()
-        // console.log('now it should jump')
-        // console.log('heres the time it should jump to')
-        // console.log(timeToPlay+10000)
-        // console.log('host time stamp', hostTimeStamp)
-        // console.log('hostProgress', hostProgress)
-        // console.log('guestTimeStamp', guestTimeStamp)
-        // }, 10000)
-
-      console.log(_this.currentSong.position)
-      setInterval(function(){console.log(_this.currentSong.position)}, 100)
-
-    }
-
   this.sendHostTimestamps = function(){
     socket.emit('hostClickedPlay',
       {timestamp: Date.now(), songProgress: _this.currentSong.position}
-    )}
-
+      )
+      var timeToPlay = (guestTimeStamp - hostTimeStamp + hostProgress)
+      _this.currentSong.setPosition((timeToPlay+10000)).play()
+      _this.currentSong.toggleMute()
+    setInterval(function(){
+      console.log(_this.currentSong.position)
+    }, 100)
+  }
+  this.sendHostTimestamps = function(){
+    socket.emit('hostClickedPlay', {timestamp: Date.now(), songProgress: _this.currentSong.position})
+  }
   this.playTrack = function() {
-
-    console.log('this is in playTrack')
     _this.currentSong.play();
     setInterval(function(){console.log(_this.currentSong.position)}, 100)
     $('#play-button').hide()
     $('#pause-button').show()
     // socket.emit('hostPickedSong', {song: _this.currentSongUri})
-
   }
-      this.pauseTrack = function() {
-        console.log('this is in pauseTrack')
-        _this.currentSong.pause()
-        $('#pause-button').hide()
-        $('#play-button').show()
-      };
-    }
+  this.pauseTrack = function() {
+    console.log('this is in pauseTrack')
+    _this.currentSong.pause()
+    $('#pause-button').hide()
+    $('#play-button').show()
+  };
+}
