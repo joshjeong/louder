dd6078d8901e7f2f9417d1a5706353d8458c5b53
Apprentice <apprentice@dbc44.local>
2014-07-30 14:02:45 -0700

    refactored all application js logic There are now only named function, there are namespaces around functions that are later to be attached to objects.

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,8 @@
+$(document).ready(function(){
+  View.hideAll()
+})
+
+
 timestampData = {}
 var serverBaseUrl = document.domain;
 socket = io.connect(serverBaseUrl);
@@ -6,62 +11,36 @@ var sessionId = '';
 var songTime = 0
 var currentSong = ""
 
+function Client(){
+  this.sessionId = ''
+  this.CurrentSongUrl = ''
+}
+
+Client.prototype = {
+  isHost: function(data){
+    return sessionId == data.participants[0].id
+  }
+}
+
+
 socket.on('connect', emitNewUser)
-socket.on('newConnection', newConnection )
-// When the server emits a new connection message, it passes the participants array
-// take the participants array and use the helper method to update the page
+
 function emitNewUser(){
   sessionId = socket.io.engine.id;
   socket.emit('newUser', {id: sessionId, name: $('#name').val(), song: "", timestamp: 0, currentProgress: 0, playing: false});
 }
 
+socket.on('newConnection', newConnection )
 
 function newConnection(data) {
-    // checks to see if this client is the host, if so, show controlls & search
-    if (sessionId == data.participants[0].id) {
-      $('#player').show()
-      $('#connect-button').hide()
-      $('#wait-screen').hide()
-      $('#guest-playing').hide()
-    }
-    // if client is a guest, check these conditions
-    if (sessionId != data.participants[0].id) {
-      // first, hide the player
-      $('#player').hide()
-
-      // second, set global song and timestamp info
-      console.log ('when a new user comes in, this is the host data')
-      console.log(data.participants[0])
-      globalCurrentSongUrl = data.participants[0].song
-      timestampData.timestamp = data.participants[0].timestamp
-      timestampData.songProgress = data.participants[0].songProgress
-      // now, check to see if host is playing
-      if (data.participants[0].playing == false) {
-        // if host isn't playing, show waiting screen
-        // debugger
-      $('#connect-button').hide()
-      $('#wait-screen').show()
-      setInterval(function(){
-        $('#wait-screen').find('h3').css( "margin-top", function( index ) {
-        return (Math.floor(Math.random()*300));})
-        $('#wait-screen').find('h3').css( "margin-left", function( index ) {
-        return (Math.floor(Math.random()*250));})
-      }, 2000)
-      $('#guest-playing').hide()
-      }
-      // if host is playing,
-      else if (data.participants[0].playing == true) {
-        if ($.grep(data.participants, function(e){ return e.id == sessionId})[0].playing == false) {
-          $('#connect-button').show()
-          $('#wait-screen').hide()
-          $('#guest-playing').hide()
-        }
-        else {
-          return
-        }
-      }
-    }
+  if (Helper.isHost(data)) {
+    View.hideAll()
+    View.showPlayer()
+  } else {
+    View.hideAll()
+    View.changeView(data)
   }
+}
 
 socket.on('songReadyForGuests', guestReadySong)
 
@@ -80,7 +59,66 @@ function readyToConect(data){
   }
 }
 
-// when the server emits an error message, log it to the console
 socket.on('error', function(reason) {
   console.log('unable to connect to server sry bro', reason);
 });
+
+
+
+Helper = {}
+//change to client soon
+Helper.isHost = function(data){
+  console.log("Is host ", sessionId == data.participants[0].id)
+  return sessionId == data.participants[0].id
+}
+Helper.isHostPlaying = function(data){
+  return data.participants[0].playing
+}
+
+
+
+
+View = {}
+
+View.changeView = function(data){
+  globalCurrentSongUrl = data.participants[0].song
+  timestampData.timestamp = data.participants[0].timestamp
+  timestampData.songProgress = data.participants[0].songProgress
+  var isClientPlaying = $.grep(data.participants, function(e){ return e.id == sessionId})[0].playing
+  if (!Helper.isHostPlaying(data)) {
+    View.showWaitScreen()
+  }else if(Helper.isHostPlaying(data) && !isClientPlaying){
+    $('#connect-button').show()
+    $('#wait-screen').hide()
+    $('#guest-playing').hide()
+  }
+}
+
+View.showPlayer = function(){
+  if(!$('#player').is(":visible")) {
+    $('#player').show()
+  }
+}
+
+View.showWaitScreen= function(){
+  if(!$('#wait-screen').is(":visible")) {
+    $('#wait-screen').show()
+    View.waitTextAnimator()
+  }
+}
+
+View.hideAll= function(){
+  $('#connect-button').hide()
+  $('#wait-screen').hide()
+  $('#guest-playing').hide()
+  $('#player').hide()
+}
+
+View.waitTextAnimator = function(){
+  setInterval(function(){
+  $('#wait-screen').find('h3').css( "margin-top", function( index ) {
+    return (Math.floor(Math.random()*300));})
+  $('#wait-screen').find('h3').css( "margin-left", function( index ) {
+    return (Math.floor(Math.random()*250));})
+  }, 2000)
+}
