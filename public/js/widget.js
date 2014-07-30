82cd6730add326e37feda4e30fbaeeeb0d7c2521
Apprentice <apprentice@dbc44.local>
2014-07-30 16:12:08 -0700

    Refactored application js and widget js to be more object oriented

--- public/js/widget.js
+++ public/js/widget.js
@@ -1,204 +1,185 @@
 timestampData = {}
 globalCurrentSongUrl = ""
+
+Player = {}
+Player.Controller = function() {
+  this.widget = {}
+  this.currentSongUri = {}
+  _this = this;
+
+  this.bindListeners = function() {
+    $("#connect-button").on('click', this.bufferGuestTrack);
+    $('#play-button').on('click', this.playTrack);
+    $('#pause-button').on('click', this.pauseTrack);
+    $('#soundCloudURL').on('submit', this.loadSongFromURL);
+  }
+
+  this.loadSongFromURL = function() {
+    var trackUrl = _this.currentSongUri
+    PlayerView.changePlayerHeight();
+    var soundCloudGetRoute = 'http://api.soundcloud.com/resolve.json?url=' + trackUrl + '&client_id=' + CLIENT_ID
+    $.get(soundCloudGetRoute, this.injectWidget)
+  }
+
+  this.injectWidget = function (song) {
+    _this.currentSongUri = song.uri;
+    _this.createWidget();
+    $(document).trigger("newSong", [_this.currentSongUri])
+    _this.bindHostWidgetListeners();
+  }
+
+  this.bufferGuestTrack = function() {
+    $(document).trigger("newGuest")
+    PlayerView.showPlay();
+    _this.currentSongUri = globalCurrentSongUrl;
+    _this.createWidget();
+    _this.bindGuestWidgetListeners();
+  }
+
+  this.sendHostTimestamps = function(){
+    //SC player
+    _this.widget.getPosition(function(position){
+      $(document).trigger("sendHostClickedPlay",
+       [Date.now(), position])
+    });
+  }
+
+  //SC Player
+  this.playTrack = function() {
+    _this.widget.play();
+    PlayerView.showPause();
+  }
+
+  //SC Player
+  this.pauseTrack = function() {
+    _this.widget.pause();
+    PlayerView.showPlay();
+  }
+
+  //SC widget
+  this.SCshowTrackTitle = function(){
+    var songInfo = _this.currentSongUri + ".json?client_id=" + CLIENT_ID;
+
+    SC.get(songInfo, function(track) {
+      console.log(track)
+      PlayerView.showTrackTitle(track.title);
+    });
+  }
+
+  this.createWidget = function(){
+    PlayerView.showWidget();
+    //managed in SC Player
+    _this.widget = SC.Widget(document.getElementById('sc-widget'));
+    //SCPlayer
+    _this.SCshowTrackTitle()
+  }
+
+  //SC Player
+  this.bindHostWidgetListeners = function(){
+    _this.widget.bind(SC.Widget.Events.READY, function(){
+      _this.bindHostPlay();
+    });
+  }
+
+  //SC Player
+  this.bindHostPlay = function(){
+    _this.widget.bind(SC.Widget.Events.PLAY, function(){
+      setTimeout(function(){
+        _this.sendHostTimestamps();
+        _this.widget.unbind(SC.Widget.Events.PLAY_PROGRESS);
+      }, 100);
+    });
+  }
+
+  //SC Player
+  this.bindGuestWidgetListeners = function(){
+    _this.widget.bind(SC.Widget.Events.READY, function(){
+      _this.bindGuestPlay();
+    });
+  }
+
+  //SC Player
+  this.bindGuestPlay = function(){
+    _this.widget.bind(SC.Widget.Events.PLAY, function() {
+      _this.widget.seekTo(timestampData.songProgress + (new Date().getTime() - timestampData.timestamp)+1900);
+      _this.widget.pause();
+      setTimeout(function(){
+        _this.widget.play();
+        _this.widget.seekTo(timestampData.songProgress + (new Date().getTime() - timestampData.timestamp));
+      }, 2000);
+      _this.widget.unbind(SC.Widget.Events.PLAY);
+    });
+  }
+
+  // This is Val's job.
+  this.setupTypeAhead = function() {
+    $('.search-field').typeahead(
+    {
+      hint: false,
+      highlight: true,
+      minLength: 1 },
+    {
+      name: 'tracks',
+      displayKey: 'title',
+      source: function (query, process) {
+        var fetch = function(query) {
+          var fetchTracks = new $.Deferred();
+          SC.get('/tracks', {q:query}, function(tracks) {
+            fetchTracks.resolve(tracks)
+          })
+          return fetchTracks.promise();
+        }
+        var dataPromise = fetch(query);
+        $('body').addClass('waiting')
+        dataPromise.done(function(tracks){
+          $('body').removeClass('waiting')
+          process(tracks);
+        });
+      },
+    }).bind("typeahead:selected", function(event, track, name){
+        _this.currentSongUri = track.uri
+        _this.loadSongFromURL()
+      })
+  };
+}
+
+PlayerView = {}
+PlayerView.showPlay= function(){
+  $('#guest-playing').show();
+  $('#pause-button').hide();
+  $('#connect-button').hide();
+}
+
+PlayerView.changePlayerHeight = function() {
+  $('#player').animate({height: "15rem"}, 1000)
+}
+
+PlayerView.showPause = function(){
+  $('#play-button').hide()
+  $('#pause-button').show()
+}
+
+PlayerView.showWidget = function(){
+  var widgetFirstHalf = "<iframe id='sc-widget' src='http://w.soundcloud.com/player/?url=";
+  var widgetSecondHalf = "&client_id=" + CLIENT_ID + "'></iframe>";
+  $('#button').show();
+  $("div#widget").html(widgetFirstHalf + _this.currentSongUri + widgetSecondHalf);
+}
+
+PlayerView.showTrackTitle = function(title){
+  $('#player').append("<div class = 'title'>" + title + "</div>");
+}
+
+
 $( document ).ready(function(){
   SC.initialize({
     client_id: CLIENT_ID,
     redirect_uri: "http://localhost:8080/",
   });
-  Player = {}
-
-  guestAnimation();
-  Player.Controller = function() {
-    this.widget = {}
-    this.currentSongUri = {}
-    _this = this;
-
-    this.bindListeners = function() {
-      $("#connect-button").on('click', this.bufferGuestTrack);
-      $('#play-button').on('click', this.playTrack);
-      $('#pause-button').on('click', this.pauseTrack);
-      $('#soundCloudURL').on('submit', this.loadSongFromURL);
-    }
-
-    this.loadSongFromURL = function() {
-      var trackUrl = _this.currentSongUri
-      _this.changePlayerHeight();
-      $.get('http://api.soundcloud.com/resolve.json?url=' + trackUrl + '&client_id=d8eb7a8be0cc38d451a51d4d223ee84b',
-      function (song) {
-        console.log(song.uri)
-        _this.currentSongUri = song.uri;
-        _this.createWidget();
-        $(document).trigger("newSong", [_this.currentSongUri])
-        _this.bindHostWidgetListeners();
-      });
-    }
-
-    //View
-    this.showPlay = function() {
-      $('#guest-playing').show();
-      $('#pause-button').hide();
-      $('#connect-button').hide();
-    }
-
-    this.bufferGuestTrack = function() {
-      //Socket
-      $(document).trigger("newGuest")
-      //View
-      _this.showPlay();
-      _this.currentSongUri = globalCurrentSongUrl;
-      console.log(globalCurrentSongUrl)
-      //View
-      _this.createWidget();
-      _this.bindGuestWidgetListeners();
-    }
-
-    //Socket
-    // this.emitTimestamps = function(timestamp, songProgress){
-    //   socket.emit('hostClickedPlay',
-    //   {timestamp: timestamp, songProgress: songProgress});
-    // }
-
-    this.sendHostTimestamps = function(){
-      //SC player
-      _this.widget.getPosition(function(position){
-        $(document).trigger("sendHostClickedPlay",
-         [Date.now(), position])
-      });
-    }
-
-    //View
-    this.changePlayerHeight = function() {
-      $('#player').animate({height: "15rem"}, 1000)
-    }
-
-    //View
-    this.showPause = function(){
-      $('#play-button').hide()
-      $('#pause-button').show()
-    }
-
-    //SC Player
-    this.playTrack = function() {
-      _this.widget.play();
-
-      //View
-      _this.showPause();
-    }
-
-    //SC Player
-    this.pauseTrack = function() {
-      _this.widget.pause();
-
-      //View
-      _this.showPlay();
-    }
-
-    //View
-    this.showWidget = function(){
-      var widgetFirstHalf = "<iframe id='sc-widget' src='http://w.soundcloud.com/player/?url=";
-      var widgetSecondHalf = "&client_id=" + CLIENT_ID + "'></iframe>";
-      $('#button').show();
-      $("div#widget").html(widgetFirstHalf + _this.currentSongUri + widgetSecondHalf);
-    }
-
-    //SC widget
-    this.SCshowTrackTitle = function(){
-      var songInfo = _this.currentSongUri + ".json?client_id=" + CLIENT_ID;
-
-      SC.get(songInfo, function(track) {
-        console.log(track)
-        _this.showTrackTitle(track.title);
-      });
-    }
-
-    this.createWidget = function(){
-      //View
-      _this.showWidget();
-
-      //managed in SC Player
-      _this.widget = SC.Widget(document.getElementById('sc-widget'));
-
-      //SCPlayer
-      _this.SCshowTrackTitle()
-    }
-
-    //View
-    this.showTrackTitle = function(title){
-      $('#player').append("<div class = 'title'>" + title + "</div>");
-    }
-
-    //SC Player
-    this.bindHostWidgetListeners = function(){
-      _this.widget.bind(SC.Widget.Events.READY, function(){
-        _this.bindHostPlay();
-      });
-    }
-
-    //SC Player
-    this.bindHostPlay = function(){
-      _this.widget.bind(SC.Widget.Events.PLAY, function(){
-        setTimeout(function(){
-          _this.sendHostTimestamps();
-          _this.widget.unbind(SC.Widget.Events.PLAY_PROGRESS);
-        }, 100);
-      });
-    }
-
-    //SC Player
-    this.bindGuestWidgetListeners = function(){
-      _this.widget.bind(SC.Widget.Events.READY, function(){
-        _this.bindGuestPlay();
-      });
-    }
-
-    //SC Player
-    this.bindGuestPlay = function(){
-      _this.widget.bind(SC.Widget.Events.PLAY, function() {
-        _this.widget.seekTo(timestampData.songProgress + (new Date().getTime() - timestampData.timestamp)+1900);
-        _this.widget.pause();
-        setTimeout(function(){
-          _this.widget.play();
-          _this.widget.seekTo(timestampData.songProgress + (new Date().getTime() - timestampData.timestamp));
-        }, 2000);
-        _this.widget.unbind(SC.Widget.Events.PLAY);
-      });
-    }
-
-    // This is Val's job.
-    this.setupTypeAhead = function() {
-      $('.search-field').typeahead(
-      {
-        hint: false,
-        highlight: true,
-        minLength: 1 },
-      {
-        name: 'tracks',
-        displayKey: 'title',
-        source: function (query, process) {
-          var fetch = function(query) {
-            var fetchTracks = new $.Deferred();
-            SC.get('/tracks', {q:query}, function(tracks) {
-              fetchTracks.resolve(tracks)
-            })
-            return fetchTracks.promise();
-          }
-          var dataPromise = fetch(query);
-          $('body').addClass('waiting')
-          dataPromise.done(function(tracks){
-            $('body').removeClass('waiting')
-            process(tracks);
-          });
-        },
-      }).bind("typeahead:selected", function(event, track, name){
-          _this.currentSongUri = track.uri
-          _this.loadSongFromURL()
-        })
-    };
-  }
-
   pController = new Player.Controller;
   pController.bindListeners();
   pController.setupTypeAhead();
+  guestAnimation();
 });
 
 // D3 OBJECT --------------------------------------
